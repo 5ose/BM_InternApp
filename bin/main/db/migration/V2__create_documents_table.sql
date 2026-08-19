d8e4fc13d471ef9709f87f4c31b0cfb83278802e
5ose <s-youssef.kamel@zewailcity.edu.eg>
2026-08-19 14:56:44 +0300

    Remove generated build files

--- bin/main/db/migration/V2__create_documents_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE documents (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    original_filename VARCHAR(255) NOT NULL,
-    file_size BIGINT NOT NULL,
-    content_type VARCHAR(255) NOT NULL,
-    storage_path VARCHAR(512) NOT NULL,
-    upload_time DATETIME(6) NOT NULL
-);
